1c7e2c1e205fddce887c9453980104a54d49b0c6
fapaholic <jaspervincem.99@gmail.com>
2024-03-28 21:29:44 +0800

    color color

--- pages/activity1/btn.js
+++
@@ -1,5 +0,0 @@
-let x = document.getElementById("btn4")
-x.addEventListener("onclick", function(){ 
-    this.hide();
-});
-
--- pages/activity1/info.js
+++ pages/activity1/info.js
@@ -5,10 +5,10 @@ const leftDiv = (title1, title2, par1, par2, button1, button2, button3, button4)
                      <p>${par2}</p>
                      <h1>${title2}</h1>
                      <p>${par1}</p>
-                     <button>${button1}</button>
-                     <button>${button2}</button>
-                     <button>${button3}</button>
-                     <button>${button4}</button>
+                     <button><span></span>${button1}</button>
+                     <button><span></span>${button2}</button>
+                     <button><span></span>${button3}</button>
+                     <button><span></span>${button4}</button>
     `
     return div;
 }
